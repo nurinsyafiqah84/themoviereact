83ffa12926c6f647438bfcedc6d0fbd7bc863586
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-04 10:16:10 +0800

    4/11

--- src/pages/Leaderboad.js
+++ src/pages/Leaderboad.js
@@ -13,12 +13,12 @@ const Leaderboad = () => {
                         <li>Edits This Week</li>
                     </ul>
                 </div>
-                <div className='h-auto grid grid-cols-2 content-start '>
-                    <div className='h-auto grid grid-rows-2 justify-center m-5 w-6/2'>
+                <div className='h-auto grid grid-cols-2 content-start p-6'>
+                    <div className='h-auto grid grid-rows-2 justify-center m-10 w-10/2'>
                         {/*setiap details*/}
-                        <div className='h-auto grid grid-cols-2 content-start '>
+                        <div className='h-auto grid grid-cols-2 content-start'>
                             {/* gambar profile*/}
-                            <div className='m-5'>
+                            <div className='m-5 w-96 ml-64 pt-2'>
                                 <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">
                                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='rounded-full w-12 h-12 align-middle border-none' ></img>
                                 </a>
@@ -26,18 +26,18 @@ const Leaderboad = () => {
                             {/* details */}
                             <ul>
                                 <li  className='mt-5'>
-                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel</button></a>
+                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel rqphqel</button></a>
                                 </li>
                                 <li>
-                                    <div className='h-5 grid grid-cols-2 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
+                                    <div className='h-5 grid grid-cols-2 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                     
                                 </li>
                                 <li>
-                                <div className='h-5 grid grid-cols-2 gap-0 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
+                                <div className='h-5 grid grid-cols-2 gap-0 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                 </li>
@@ -47,7 +47,7 @@ const Leaderboad = () => {
                         {/*setiap details*/}
                         <div className='h-auto grid grid-cols-2 content-start '>
                             {/* gambar profile*/}
-                            <div className='m-5'>
+                            <div className='m-55 w-96 ml-64 mt-6'>
                                 <a href="https://www.themoviedb.org/u/buszmen201" target="_blank" rel="noreferrer">
                                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='rounded-full w-12 h-12 align-middle border-none' ></img>
                                 </a>
@@ -55,28 +55,28 @@ const Leaderboad = () => {
                             {/* details */}
                             <ul>
                                 <li  className='mt-5'>
-                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">rqphqel</a>
+                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">rqphqel rqphqel</a>
                                 </li>
                                 <li>
-                                    <div className='h-5 grid grid-cols-2 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
+                                    <div className='h-5 grid grid-cols-2 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                     
                                 </li>
                                 <li>
-                                <div className='h-5 grid grid-cols-2 gap-0 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
+                                <div className='h-5 grid grid-cols-2 gap-0 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                 </li>
                             </ul>
                         </div>
 
-                        {/*setiap details*/}
-                        <div className='h-auto grid grid-cols-2 content-start '>
+                                                {/*setiap details*/}
+                                                <div className='h-auto grid grid-cols-2 content-start'>
                             {/* gambar profile*/}
-                            <div className='m-5'>
+                            <div className='m-5 w-96 ml-64 pt-2'>
                                 <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">
                                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='rounded-full w-12 h-12 align-middle border-none' ></img>
                                 </a>
@@ -84,28 +84,28 @@ const Leaderboad = () => {
                             {/* details */}
                             <ul>
                                 <li  className='mt-5'>
-                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel</button></a>
+                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel rqphqel</button></a>
                                 </li>
                                 <li>
-                                    <div className='h-5 grid grid-cols-2 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
+                                    <div className='h-5 grid grid-cols-2 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                     
                                 </li>
                                 <li>
-                                <div className='h-5 grid grid-cols-2 gap-0 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
+                                <div className='h-5 grid grid-cols-2 gap-0 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                 </li>
                             </ul>
                         </div>
 
-                        {/*setiap details*/}
-                        <div className='h-auto grid grid-cols-2 content-start '>
+                                                {/*setiap details*/}
+                                                <div className='h-auto grid grid-cols-2 content-start'>
                             {/* gambar profile*/}
-                            <div className='m-5'>
+                            <div className='m-5 w-96 ml-64 pt-2'>
                                 <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">
                                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='rounded-full w-12 h-12 align-middle border-none' ></img>
                                 </a>
@@ -113,28 +113,28 @@ const Leaderboad = () => {
                             {/* details */}
                             <ul>
                                 <li  className='mt-5'>
-                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel</button></a>
+                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel rqphqel</button></a>
                                 </li>
                                 <li>
-                                    <div className='h-5 grid grid-cols-2 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
+                                    <div className='h-5 grid grid-cols-2 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                     
                                 </li>
                                 <li>
-                                <div className='h-5 grid grid-cols-2 gap-0 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
+                                <div className='h-5 grid grid-cols-2 gap-0 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                 </li>
                             </ul>
                         </div>
 
-                        {/*setiap details*/}
-                        <div className='h-auto grid grid-cols-2 content-start '>
+                                                {/*setiap details*/}
+                        <div className='h-auto grid grid-cols-2 content-start'>
                             {/* gambar profile*/}
-                            <div className='m-5'>
+                            <div className='m-5 w-96 ml-64 pt-2'>
                                 <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer">
                                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='rounded-full w-12 h-12 align-middle border-none' ></img>
                                 </a>
@@ -142,18 +142,18 @@ const Leaderboad = () => {
                             {/* details */}
                             <ul>
                                 <li  className='mt-5'>
-                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel</button></a>
+                                    <a href="https://www.themoviedb.org/u/rqphqel" target="_blank" rel="noreferrer"><button >rqphqel rqphqel</button></a>
                                 </li>
                                 <li>
-                                    <div className='h-5 grid grid-cols-2 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
+                                    <div className='h-5 grid grid-cols-2 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-lightGreen text-xs font-bold mr-1  h-2 w-3/6"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                     
                                 </li>
                                 <li>
-                                <div className='h-5 grid grid-cols-2 gap-0 content-start'>
-                                        <div class="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
+                                <div className='h-5 grid grid-cols-2 gap-0 content-start w-full'>
+                                        <div className="mt-1 rounded-full text-white bg-darkRed text-xs font-bold mr-1  h-2 w-full"></div>
                                         <div className='font text-xs'>62,187</div>
                                     </div>
                                 </li>
